8e44287dfe07d288c1b142096e8487d5a38db12f
SeanMcKeen <SeanTFD@gmail.com>
2024-02-02 13:10:04 -0500

    Fixed it after breaking it + added some debugging stuff

--- rarity.py
+++ rarity.py
@@ -27,6 +27,13 @@ while playing:
     try:
         plrInput = int(plrInput)
         numToRoll = plrInput
+
+        option1Occured = 0
+        option2Occured = 0
+        option3Occured = 0
+        option4Occured = 0
+        option5Occured = 0
+
         for i in range(numToRoll):
             prevTotals = 0
             numPick = random.uniform(1, totalNum)
@@ -34,16 +41,27 @@ while playing:
                 if numPick <= value + prevTotals:
                     if key == "common":
                         prize = random.choice(common)
+                        option1Occured += 1
                     elif key == "uncommon":
                         prize = random.choice(uncommon)
+                        option2Occured += 1
                     elif key == "rare":
                         prize = random.choice(rare)
+                        option3Occured += 1
                     elif key == "legendary":
                         prize = random.choice(legendary)
+                        option4Occured += 1
                     elif key == "mythical":
                         prize = random.choice(mythical)
-                    print(prize)
+                        option5Occured += 1
+                    break
                 prevTotals += value
+        print("Option 1 Occured:", option1Occured, "| Real Percentage:", (option1Occured/numToRoll)*100, "%")
+        print("Option 2 Occured:", option2Occured, "| Real Percentage:", (option2Occured/numToRoll)*100, "%")
+        print("Option 3 Occured:", option3Occured, "| Real Percentage:", (option3Occured/numToRoll)*100, "%")
+        print("Option 4 Occured:", option4Occured, "| Real Percentage:", (option4Occured/numToRoll)*100, "%")
+        print("Option 5 Occured:", option5Occured, "| Real Percentage:", (option5Occured/numToRoll)*100, "%")
+            
     except:
         if plrInput == "stop":
             playing = False
